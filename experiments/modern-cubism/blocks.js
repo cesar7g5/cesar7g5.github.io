52e9679787a929f7ea602dda87b2acab4d435f2c
Cesar Chavez <cesar7g5@gmail.com>
2023-11-13 19:04:14 +0100

    Update blocks.js

--- experiments/modern-cubism/blocks.js
+++ experiments/modern-cubism/blocks.js
@@ -6,7 +6,7 @@ function BlocksSketch(p) {
   const BASE_HUE_RANGE = 300;
   const HUE_RANGE = 180;
   const MIN_BLOCKS_PER_ROW = 6;
-  const MAX_BLOCKS_PER_ROW = 25;
+  const MAX_BLOCKS_PER_ROW = 22;
 
   const MIN_BLOCK_SIZE = CANVAS_WIDTH / MAX_BLOCKS_PER_ROW;
   const MAX_BLOCK_SIZE = CANVAS_WIDTH / MIN_BLOCKS_PER_ROW;
